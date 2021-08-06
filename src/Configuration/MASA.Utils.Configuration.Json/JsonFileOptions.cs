ba2917ad005608f83e1686e7f154884a68b07183
dodd <358683537@qq.com>
2021-08-06 17:02:03 +0800

    feat: add MASA.Utils.Configuration

---
+++ src/Configuration/MASA.Utils.Configuration.Json/JsonFileOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASA.Utils.Configuration.Json
+{
+    public class JsonFileOptions
+    {
+        /// <summary>
+        /// Path relative to the base path stored in Microsoft.Extensions.Configuration.IConfigurationBuilder.Properties of builder.
+        /// </summary>
+        public string? FileName { get; set; }
+
+        /// <summary>
+        /// Whether the file is optional.
+        /// </summary>
+        public bool Optional { get; set; }
+
+        /// <summary>
+        ///  Whether the configuration should be reloaded if the file changes.
+        /// </summary>
+        public bool ReloadOnChange { get; set; }
+    }
+}
