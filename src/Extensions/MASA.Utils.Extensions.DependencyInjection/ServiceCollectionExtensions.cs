4ab9db86aa8166b4c76d5a63bd6fcac4e6cc9502
dodd <358683537@qq.com>
2021-09-08 16:05:23 +0800

    feat: add ServiceCollectionExtensions

---
+++ src/Extensions/MASA.Utils.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection;
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Auto add all service to IoC, lifecycle is scoped
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="suffix">default is Service</param>
+    public static IServiceCollection AddServices(this IServiceCollection services, string suffix = "Service", bool autoFire = false)
+    {
+        Assembly
+            .GetCallingAssembly()
+            .GetTypes()
+            .Where(t => t.Name.EndsWith(suffix))
+            .ToList()
+            .ForEach(serviceType => AddScoped(services, serviceType, autoFire));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Auto add all service to IoC, lifecycle is scoped
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="suffix">default is Service</param>
+    public static IServiceCollection AddServices<TService>(this IServiceCollection services, bool autoFire = false)
+    {
+        var serviceType = typeof(TService);
+
+        Assembly
+            .GetCallingAssembly()
+            .GetTypes()
+            .Where(t => t.BaseType == serviceType)
+            .ToList()
+            .ForEach(serviceType => AddScoped(services, serviceType, autoFire));
+
+        return services;
+    }
+
+    private static void AddScoped(IServiceCollection services, Type serviceType, bool autoFire)
+    {
+        services.AddScoped(serviceType);
+
+        if (autoFire) services.BuildServiceProvider().GetService(serviceType);
+    }
+}
