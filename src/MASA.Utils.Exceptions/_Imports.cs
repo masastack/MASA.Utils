c8ca2598e4d21e849e2f97d1e93a80c4a47e5267
zhenlei520 <wangzhenlei520@gmail.com>
2022-01-17 16:47:14 +0800

    chore: adjust package

--- src/MASA.Utils.Exceptions/_Imports.cs
+++ src/MASA.Utils.Exceptions/_Imports.cs
@@ -1,7 +1,12 @@
 global using MASA.Utils.Exceptions.Handling;
 global using MASA.Utils.Exceptions.Handling.Extensions;
+global using MASA.Utils.Exceptions.Results;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Filters;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using System.Net;
 global using System.Runtime.Serialization;
