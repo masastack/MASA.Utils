7d0e6a221973e82e3d59f24aa53d829a749a2639
zhenlei520 <wangzhenlei520@gmail.com>
2022-02-20 23:57:18 +0800

    feature: add dapr background options

--- src/Development/MASA.Utils.Development.Dapr.AspNetCore/DaprBackgroundService.cs
+++ src/Development/MASA.Utils.Development.Dapr.AspNetCore/DaprBackgroundService.cs
@@ -5,12 +5,14 @@ public class DaprBackgroundService : IHostedService
     private readonly IServiceProvider _serviceProvider;
     private readonly IDaprProcess _daprProcess;
     private readonly DaprOptions _options;
+    private readonly DaprBackgroundOptions _daprBackgroundOptions;
     private readonly ILogger<DaprBackgroundService>? _logger;
 
     public DaprBackgroundService(
         IServiceProvider serviceProvider,
         IDaprProcess daprProcess,
         IOptionsMonitor<DaprOptions> options,
+        IOptions<DaprBackgroundOptions> daprBackgroundOptions,
         ILogger<DaprBackgroundService>? logger)
     {
         _serviceProvider = serviceProvider;
@@ -21,23 +23,31 @@ public class DaprBackgroundService : IHostedService
             daprOptions.AppPort ??= GetAppPort(daprOptions);
             _daprProcess.Refresh(daprOptions);
         });
+        _daprBackgroundOptions = daprBackgroundOptions.Value;
         _logger = logger;
     }
 
     private ushort GetAppPort(DaprOptions options)
     {
+        int retry = 0;
+        again:
         var server = _serviceProvider.GetRequiredService<IServer>();
         var addresses = server.Features.Get<IServerAddressesFeature>()?.Addresses;
-        if (addresses != null && !addresses.IsReadOnly && addresses.Count == 0)
+        if (addresses is { IsReadOnly: false, Count: 0 })
         {
+            if (retry < _daprBackgroundOptions.MaxRetryTimes)
+            {
+                _logger?.LogWarning("dapr: failed to get app running port, retrying, please wait...");
+                retry++;
+                goto again;
+            }
             throw new Exception("Failed to get the startup port, please specify the port manually");
         }
 
         return addresses!
             .Select(address => new Uri(address))
             .Where(address
-                => (options.EnableSsl != null && options.EnableSsl == true &&
-                    address.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
+                => (options.EnableSsl is true && address.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
                 || address.Scheme.Equals(Uri.UriSchemeHttp, StringComparison.OrdinalIgnoreCase))
             .Select(address => (ushort)address.Port).FirstOrDefault();
     }
@@ -45,7 +55,7 @@ public class DaprBackgroundService : IHostedService
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _logger?.LogInformation("{Name} is Starting ...", nameof(DaprBackgroundService));
-        System.Timers.Timer timer = new System.Timers.Timer(300);
+        System.Timers.Timer timer = new System.Timers.Timer(_daprBackgroundOptions.Time);
         timer.Elapsed += delegate
         {
             _options.AppPort ??= GetAppPort(_options);
--- src/Development/MASA.Utils.Development.Dapr.AspNetCore/ServiceCollectionExtensions.cs
+++ src/Development/MASA.Utils.Development.Dapr.AspNetCore/ServiceCollectionExtensions.cs
@@ -2,20 +2,28 @@
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddDapr(this IServiceCollection services, Action<DaprOptions>? action = null)
+    public static IServiceCollection AddDapr(
+        this IServiceCollection services,
+        Action<DaprOptions>? action = null,
+        Action<DaprBackgroundOptions>? daprBackgroundOptionsAction = null)
     {
         DaprOptions daprOptions = new();
         action?.Invoke(daprOptions);
-        return services.AddDapr(() => daprOptions);
+        return services.AddDapr(() => daprOptions, daprBackgroundOptionsAction);
     }
 
-    public static IServiceCollection AddDapr(this IServiceCollection services, Func<DaprOptions> func)
+    public static IServiceCollection AddDapr(this IServiceCollection services,
+        Func<DaprOptions> func,
+        Action<DaprBackgroundOptions>? action = null)
     {
         if (services.Any(service => service.ImplementationType == typeof(DaprService)))
         {
             return services;
         }
         services.AddSingleton<DaprService>();
+
+        if (action != null)
+            services.Configure(action);
 
         services.AddHostedService<DaprBackgroundService>();
         services.Configure<HostOptions>(opts => opts.ShutdownTimeout = TimeSpan.FromSeconds(15));
@@ -29,8 +37,8 @@ public static class ServiceCollectionExtensions
             return services;
         }
         services.AddSingleton<DaprService>();
-
         services.AddHostedService<DaprBackgroundService>();
+        services.Configure<DaprBackgroundOptions>(configuration);
         return services.AddDaprCore(configuration);
     }
 
