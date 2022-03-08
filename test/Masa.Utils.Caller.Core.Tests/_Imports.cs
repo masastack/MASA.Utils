c236842cd5d88ee08875081e67599f76673e4266
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-08 18:18:01 +0800

    fix: fix Caller auto configuration injection exception

---
+++ test/Masa.Utils.Caller.Core.Tests/_Imports.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Masa.Utils.Caller.HttpClient;
+global using Masa.Utils.Caller.Core.Tests.Callers;
+global using Microsoft.Extensions.DependencyInjection;
+
