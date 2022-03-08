7b797df18e708492578ff1099c025e521d5a46fc
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-08 18:34:44 +0800

    test: adjustment format

--- test/Masa.Utils.Caller.Core.Tests/_Imports.cs
+++ test/Masa.Utils.Caller.Core.Tests/_Imports.cs
@@ -1,5 +1,4 @@
-﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Masa.Utils.Caller.HttpClient;
 global using Masa.Utils.Caller.Core.Tests.Callers;
+global using Masa.Utils.Caller.HttpClient;
 global using Microsoft.Extensions.DependencyInjection;
-
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
