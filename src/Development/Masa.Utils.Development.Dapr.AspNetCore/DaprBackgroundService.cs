486a2df4cd3e47a7e2e630e24968ab7c728190fa
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-14 11:18:40 +0800

    refactor(dapr): refactor dapr background service

--- src/Development/Masa.Utils.Development.Dapr.AspNetCore/DaprBackgroundService.cs
+++ src/Development/Masa.Utils.Development.Dapr.AspNetCore/DaprBackgroundService.cs
@@ -1,18 +1,18 @@
 namespace Masa.Utils.Development.Dapr.AspNetCore;
 
-public class DaprBackgroundService : IHostedService
+public class DaprBackgroundService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly IDaprProcess _daprProcess;
     private readonly DaprOptions _options;
-    private readonly DaprBackgroundOptions _daprBackgroundOptions;
+    private readonly IHostApplicationLifetime _lifetime;
     private readonly ILogger<DaprBackgroundService>? _logger;
 
     public DaprBackgroundService(
         IServiceProvider serviceProvider,
         IDaprProcess daprProcess,
         IOptionsMonitor<DaprOptions> options,
-        IOptions<DaprBackgroundOptions> daprBackgroundOptions,
+        IHostApplicationLifetime lifetime,
         ILogger<DaprBackgroundService>? logger)
     {
         _serviceProvider = serviceProvider;
@@ -23,26 +23,16 @@ public class DaprBackgroundService : IHostedService
             daprOptions.AppPort ??= GetAppPort(daprOptions);
             _daprProcess.Refresh(daprOptions);
         });
-        _daprBackgroundOptions = daprBackgroundOptions.Value;
+        _lifetime = lifetime;
         _logger = logger;
     }
 
     private ushort GetAppPort(DaprOptions options)
     {
-        int retry = 0;
-        again:
         var server = _serviceProvider.GetRequiredService<IServer>();
         var addresses = server.Features.Get<IServerAddressesFeature>()?.Addresses;
         if (addresses is { IsReadOnly: false, Count: 0 })
-        {
-            if (retry < _daprBackgroundOptions.MaxRetryTimes)
-            {
-                _logger?.LogWarning("dapr: failed to get app running port, retrying, please wait...");
-                retry++;
-                goto again;
-            }
             throw new Exception("Failed to get the startup port, please specify the port manually");
-        }
 
         return addresses!
             .Select(address => new Uri(address))
@@ -52,25 +42,36 @@ public class DaprBackgroundService : IHostedService
             .Select(address => (ushort)address.Port).FirstOrDefault();
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _logger?.LogInformation("{Name} is Starting ...", nameof(DaprBackgroundService));
-        System.Timers.Timer timer = new System.Timers.Timer(_daprBackgroundOptions.Time);
-        timer.Elapsed += delegate
+        if (!await WaitForAppStartup(_lifetime, stoppingToken))
+            return;
+
+        // if cancellation was requested, stop
+        if (stoppingToken.IsCancellationRequested)
         {
+            _logger?.LogInformation("{Name} is Stopping...", nameof(DaprBackgroundService));
+            _daprProcess.Stop(stoppingToken);
+        }
+        else
+        {
+            _logger?.LogInformation("{Name} is Starting ...", nameof(DaprBackgroundService));
             _options.AppPort ??= GetAppPort(_options);
-            _daprProcess.Start(_options, cancellationToken);
-        };
-        timer.AutoReset = false;
-        timer.Enabled = true;
-        timer.Start();
-        return Task.CompletedTask;
+            _daprProcess.Start(_options, stoppingToken);
+        }
     }
 
-    public Task StopAsync(CancellationToken cancellationToken)
+    static async Task<bool> WaitForAppStartup(IHostApplicationLifetime lifetime, CancellationToken stoppingToken)
     {
-        _logger?.LogInformation("{Name} is Stopping...", nameof(DaprBackgroundService));
-        _daprProcess.Stop(cancellationToken);
-        return Task.CompletedTask;
+        var startedSource = new TaskCompletionSource();
+        var cancelledSource = new TaskCompletionSource();
+
+        await using var startedCancellationTokenRegistration = lifetime.ApplicationStarted.Register(() => startedSource.SetResult());
+        await using var cancellationTokenRegistration = stoppingToken.Register(() => cancelledSource.SetResult());
+
+        Task completedTask = await Task.WhenAny(startedSource.Task, cancelledSource.Task).ConfigureAwait(false);
+
+        // If the completed tasks was the "app started" task, return true, otherwise false
+        return completedTask == startedSource.Task;
     }
 }
