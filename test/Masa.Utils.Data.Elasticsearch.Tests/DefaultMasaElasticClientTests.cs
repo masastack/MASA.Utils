ba71299d9673cf4c5eb2cd0713f4eeb8b2124c2c
zhenlei520 <wangzhenlei520@gmail.com>
2022-03-24 21:03:22 +0800

    test(Elasticsearch): add Elasticsearch unit test

---
+++ test/Masa.Utils.Data.Elasticsearch.Tests/DefaultMasaElasticClientTests.cs
@@ -0,0 +1,136 @@
+﻿namespace Masa.Utils.Data.Elasticsearch.Tests;
+
+[TestClass]
+public class DefaultMasaElasticClientTests
+{
+    private MasaElasticsearchBuilder _builder;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        IServiceCollection service = new ServiceCollection();
+        _builder = service.AddElasticsearchClient("es", "http://localhost:9200");
+    }
+
+    [TestMethod]
+    public async Task CreateDocumentAsync()
+    {
+        string indexName = "user_index";
+        var countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName));
+        Assert.IsTrue(!countResponse.IsValid);
+
+        var createResponse = await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+        Assert.IsTrue(createResponse.IsValid);
+
+        Thread.Sleep(1000);
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+        await _builder.Client.DeleteIndexAsync(indexName);
+    }
+
+    [TestMethod]
+    public async Task BindAliasAsync()
+    {
+        string indexName = "user_index_1";
+        string indexName2 = "user_index_2";
+        string alias = "userIndex";
+
+        await _builder.Client.CreateIndexAsync(indexName);
+        await _builder.Client.CreateIndexAsync(indexName2);
+
+        await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+        await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName2, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+
+        Thread.Sleep(1000);
+        var countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName2));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+
+        await _builder.Client.BindAliasAsync(new BindAliasIndexOptions(alias, new[] { indexName, indexName2 }));
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(alias));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 2);
+
+        await _builder.Client.DeleteIndexAsync(indexName);
+        await _builder.Client.DeleteIndexAsync(indexName2);
+    }
+
+    [TestMethod]
+    public async Task DeleteIndexByAliasAsync()
+    {
+        string indexName = "user_index_1";
+        string indexName2 = "user_index_2";
+        string alias = "userIndex";
+
+        await _builder.Client.CreateIndexAsync(indexName);
+        await _builder.Client.CreateIndexAsync(indexName2);
+
+        await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+        await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName2, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+
+        Thread.Sleep(1000);
+        var countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName2));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+
+        await _builder.Client.BindAliasAsync(new BindAliasIndexOptions(alias, new[] { indexName, indexName2 }));
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(alias));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 2);
+
+        await _builder.Client.DeleteIndexByAliasAsync(alias);
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(alias));
+        Assert.IsTrue(!countResponse.IsValid);
+    }
+
+    [TestMethod]
+    public async Task ClearDocumentAsync()
+    {
+        string indexName = "user_index_1";
+        string indexName2 = "user_index_2";
+        string alias = "userIndex";
+
+        await _builder.Client.CreateIndexAsync(indexName);
+        await _builder.Client.CreateIndexAsync(indexName2);
+
+        await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+        await _builder.Client.CreateDocumentAsync(new CreateDocumentRequest<object>(indexName2, new
+        {
+            id = Guid.NewGuid()
+        }, Guid.NewGuid().ToString()));
+
+        Thread.Sleep(1000);
+        var countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(indexName2));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 1);
+
+        await _builder.Client.BindAliasAsync(new BindAliasIndexOptions(alias, new[] { indexName, indexName2 }));
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(alias));
+        Assert.IsTrue(countResponse.IsValid && countResponse.Count == 2);
+
+        await _builder.Client.ClearDocumentAsync(alias);
+        countResponse = await _builder.Client.DocumentCountAsync(new CountDocumentRequest(alias));
+        Assert.IsTrue(countResponse.IsValid);
+
+        await _builder.Client.DeleteIndexByAliasAsync(alias);
+    }
+}
---
+++ test/Masa.Utils.Data.Elasticsearch.Tests/_Imports.cs
@@ -0,0 +1,5 @@
+global using Masa.Utils.Data.Elasticsearch.Options.Alias;
+global using Masa.Utils.Data.Elasticsearch.Options.Document.Count;
+global using Masa.Utils.Data.Elasticsearch.Options.Document.Create;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
