62f17dc20aa1fe142578353123566e99d714a2ce
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-03-24 18:59:10 +0800

    feat:add utils for ldap with novell (#31)
    * feat:add unit test
    
    * refactor:modify code for pr
    
    * refactor:modify code for pr
    
    * chore:fix merge error
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

---
+++ src/Ldap/Masa.Utils.Ldap.Novell/_Imports.cs
@@ -0,0 +1,7 @@
+global using Masa.Utils.Ldap.Novell.Entries;
+global using Masa.Utils.Ldap.Novell.Extensions;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
+global using Novell.Directory.Ldap;
+global using System.Text;
---
+++ test/Masa.Utils.Ldap.Tests/Masa.Utils.Ldap.Novell.Tests/LdapTest.cs
@@ -0,0 +1,69 @@
+namespace Masa.Utils.Ldap.Novell.Tests;
+
+[TestClass]
+public class LdapTest
+{
+    readonly IServiceCollection Services;
+    readonly ILdapProvider ldapProvider;
+
+    public LdapTest()
+    {
+        Services = new ServiceCollection();
+        var mockLdapOptions = new Mock<LdapOptions>();
+
+        var configurationBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        var configuration = configurationBuilder.Build();
+        var ldapConfigurationSection = configuration.GetSection(nameof(LdapOptions));
+        Services.AddLadpContext(ldapConfigurationSection);
+        var serviceProvider = Services.BuildServiceProvider();
+        ldapProvider = serviceProvider.GetRequiredService<ILdapProvider>();
+    }
+
+    [TestInitialize]
+    public void EdgeDriverInitialize()
+    {
+
+    }
+
+    [TestMethod]
+    public async Task GetAllUser()
+    {
+        var allUsers = await ldapProvider.GetAllUserAsync().ToListAsync(); ;
+        Assert.IsTrue(allUsers.Count > 0);
+    }
+
+    [TestMethod]
+    public async Task GetPagingUser()
+    {
+        var pagingUsers = await ldapProvider.GetPagingUserAsync(1);
+        Assert.IsTrue(pagingUsers.Count > 0);
+    }
+
+    [TestMethod]
+    public async Task GetUserByUserName()
+    {
+        var user = await ldapProvider.GetUserByUserNameAsync("mayue");
+        Assert.IsNotNull(user);
+    }
+
+    [TestMethod]
+    public async Task GetUserByUserEmail()
+    {
+        var user = await ldapProvider.GetUsersByEmailAddressAsync("mayue@masastack.com");
+        Assert.IsNotNull(user);
+    }
+
+    [TestMethod]
+    public async Task GetGroupAsync()
+    {
+        var group = await ldapProvider.GetGroupAsync("杭州产品研发部");
+        Assert.IsNotNull(group);
+    }
+
+    [TestMethod]
+    public async Task GetUsersInGroupAsync()
+    {
+        var users = await ldapProvider.GetUsersInGroupAsync("杭州产品研发部").ToListAsync();
+        Assert.IsTrue(users.Count > 0);
+    }
+}
