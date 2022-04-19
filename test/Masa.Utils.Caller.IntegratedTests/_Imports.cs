0cef647c834e75b822eaeeb28ad8039c5e759987
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-19 09:18:45 +0800

    chore: global using sort

--- test/Masa.Utils.Caller.IntegratedTests/_Imports.cs
+++ test/Masa.Utils.Caller.IntegratedTests/_Imports.cs
@@ -1,8 +1,8 @@
 global using Masa.Utils.Caller.Core;
+global using Masa.Utils.Caller.DaprClient;
 global using Masa.Utils.Caller.HttpClient;
 global using Masa.Utils.Caller.IntegratedTest.Callers;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using System.Net;
-global using Masa.Utils.Caller.DaprClient;
