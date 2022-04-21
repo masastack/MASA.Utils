6593747e4882f39d6b9ee6981e7d939078d90c1c
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-04-21 17:45:38 +0800

    Feat:remove ToListAsync extension method (#46)
    * feat:add ldap factory
    
    * fix:remove ToListAsync
    System.Linq.AsyncEnumerable.ToListAsync ambiguity
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

--- test/Masa.Utils.Ldap.Tests/Masa.Utils.Ldap.Novell.Tests/_Imports.cs
+++ test/Masa.Utils.Ldap.Tests/Masa.Utils.Ldap.Novell.Tests/_Imports.cs
@@ -3,4 +3,5 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using Moq;
+global using System.Linq;
 global using System.Threading.Tasks;
