7a5084b4a64a0a774774c1499e9491629eb0bbbe
听雨声 <wangzhenlei520@gmail.com>
2022-05-11 09:28:07 +0800

    refactor(EntityFrameworkCore): Delete EntityFrameworkCore Library Package (#56)

--- src/Data/Masa.Utils.Data.EntityFrameworkCore.Cosmos/_Imports.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
-
-global using Masa.Utils.Data.EntityFrameworkCore.Cosmos.Internal;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Infrastructure;
-global using Microsoft.Extensions.DependencyInjection;
