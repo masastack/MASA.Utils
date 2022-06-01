885c3c4f53c038054a1be170f9eeb538d64fa8a0
Gavin <kevin_1ee@163.com>
2022-06-01 14:57:46 +0800

    rename Masa.Utils.Extensions.FluentValidation to Masa.Utils.Extensions.Validations.FluentValidation

--- src/Extensions/Validations/Masa.Utils.Extensions.Validations.FluentValidation/RegularHelper.cs
+++ src/Extensions/Validations/Masa.Utils.Extensions.Validations.FluentValidation/RegularHelper.cs
@@ -1,7 +1,7 @@
 // Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
-namespace Masa.Utils.Extensions.FluentValidation;
+namespace Masa.Utils.Extensions.Validations.FluentValidation;
 
 internal static class RegularHelper
 {
