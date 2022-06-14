c5836e9efb7e9f2ded9855e6a82c58d9a7b77c5f
qinyouzeng <qinyouzeng@masastack.com>
2022-06-14 16:47:56 +0800

    feat: add promethus http api query client

---
+++ Masa.Utils.Data.Promethus/MasaPromethusClient.cs
@@ -0,0 +1,37 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Utils.Data.Promethus;
+
+internal class MasaPromethusClient : IMasaPromethusClient
+{
+    public Task<ResponseExemplarResultModel> ExemplarQueryAsync(RequestQueryModel query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseLabelResultModel> LabelsQueryAsync(RequestMetaDataQueryModel query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseLabelResultModel> LabelValuesQueryAsync(RequestMetaDataQueryModel query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseQueryResultCommonModel> QueryAsync(RequestQueryModel query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseQueryResultCommonModel> QueryRangeAsync(RequestQueryRangeModel query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseSerieResultModel> SeriesAsync(RequestMetaDataQueryModel query)
+    {
+        throw new NotImplementedException();
+    }
+}
