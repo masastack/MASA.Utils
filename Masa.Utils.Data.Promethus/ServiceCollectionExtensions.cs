fa58ddb5f01c32127770473d5a14809033993a32
qinyouzeng <qinyouzeng@masastack.com>
2022-06-17 11:26:38 +0800

    feat: update code and add tests

---
+++ Masa.Utils.Data.Promethus/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+using Masa.Utils.Caller.Core;
+using Masa.Utils.Caller.HttpClient;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Masa.Utils.Data.Promethus;
+
+public static class ServiceCollectionExtensions
+{
+    private const string PROMETHUS_HTTP_CLIENT_NAME = "promethus_client_name";
+
+    public static IServiceCollection AddPromethusClient(this IServiceCollection services, string uri)
+    {
+        ArgumentNullException.ThrowIfNull(uri, nameof(uri));
+
+        services.AddCaller(builder =>
+        {
+            builder.UseHttpClient(options =>
+            {
+                options.BaseAddress = uri;
+                options.Name = PROMETHUS_HTTP_CLIENT_NAME;
+            });
+        });
+
+        services.AddScoped<IMasaPromethusClient, MasaPromethusClient>();
+        return services;
+    }
+}
--- test/Masa.Utils.Data.Promethus.Test/MasaPromethusClientTests.cs
+++ test/Masa.Utils.Data.Promethus.Test/MasaPromethusClientTests.cs
@@ -1,68 +1,120 @@
-using Masa.Utils.Data.Promethus.Enums;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 namespace Masa.Utils.Data.Promethus.Test
 {
     [TestClass]
     public class MasaPromethusClientTests
     {
-        private MasaPromethusClient _client;
+        private IMasaPromethusClient _client;
 
-        [TestMethod]
-        public async Task TestSeriesAsync()
+        [TestInitialize]
+        public void Initialize()
         {
-            var data = await _client.SeriesAsync(new Model.RequestMetaDataQueryModel
-            {
-                Match = null,
-
-            });
-            Assert.IsNotNull(data);
+            IServiceCollection service = new ServiceCollection();
+            service.AddPromethusClient("http://localhost:9090");
+            _client = service.BuildServiceProvider().GetService<IMasaPromethusClient>();
         }
 
         [TestMethod]
         public async Task TestQueryAsync()
         {
-            var data = await _client.SeriesAsync(new Model.RequestMetaDataQueryModel
+            var result = await _client.QueryAsync(new RequestQueryModel
             {
-                Match = null,
-
+                Query = "up"
             });
-            Assert.IsNotNull(data);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Status, ResultStatuses.Success);
+            Assert.IsNotNull(result.Data);
         }
 
         [TestMethod]
-        [DataRow()]
-        public async Task TestExemplarQueryAsync(string time, string timeOut, string query)
+        public async Task TestQueryVectorAsync()
         {
-            var param = new Model.RequestQueryModel
+            var result = await _client.QueryAsync(new RequestQueryModel
             {
-                Query = query,
-                Time = time,
-                TimeOut = timeOut
-            };
-            var data = await _client.ExemplarQueryAsync(param);
-            if (string.IsNullOrEmpty(query))
-                Assert.AreEqual(ResultStatuses.Error, data.Status);
+                Query = "up"
+            });
+
+            if (result.Data.Result != null)
+            {
+                var data = result.Data.Result as InstantVectorModel[];
+
+                Assert.IsNotNull(data);
+                Assert.IsNotNull(data[0].Metric);
+                Assert.IsNotNull(data[0].Value);
+
+                Assert.IsNotNull(data[0].Metric.Keys);
+                Assert.AreEqual(2, data[0].Value.Length);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestQueryRangeAsync()
+        {
+            var result = await _client.QueryRangeAsync(new RequestQueryRangeModel
+            {
+                Query = "up",
+                Start = "2022-06-17T02:00:00.000Z",
+                End = "2022-06-17T02:30:00.000Z",
+                Step = "300s",
+            });
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Status, ResultStatuses.Success);
+            Assert.IsNotNull(result.Data);
+            if (result.Data.ResultType == ResultTypes.Matrix)
+            {
+                var data = result.Data.Result as MatrixRangeModel[];
+                Assert.IsNotNull(data);
+                Assert.IsNotNull(data[0].Metric);
+                Assert.IsNotNull(data[0].Values);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestSeriesAsync()
+        {
+            var result = await _client.SeriesAsync(new RequestMetaDataQueryModel
+            {
+                Match = new string[] { "up" },
+                Start = "2022-06-17T02:00:00.000Z",
+                End = "2022-06-17T02:30:00.000Z"
+            });
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Status, ResultStatuses.Success);
         }
 
         [TestMethod]
         public async Task TestLabelsQueryAsync()
         {
-            await Task.CompletedTask;
+            var result = await _client.LabelsQueryAsync(default!);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Status, ResultStatuses.Success);
+            Assert.IsTrue(result.Data.Count() > 0);
         }
 
         [TestMethod]
         public async Task TestLabelValuesQueryAsync()
         {
-            await Task.CompletedTask;
+            var result = await _client.LabelValuesQueryAsync(new RequestLableValueQueryModel());
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Status, ResultStatuses.Success);
+            Assert.IsTrue(result.Data.Count() > 0);
         }
 
-
         [TestMethod]
-        public async Task TestQueryRangeAsync()
+        [DataRow()]
+        public async Task TestExemplarQueryAsync()
         {
-            await Task.CompletedTask;
+            var param = new RequestQueryExemplarModel
+            {
+                Query = "up",
+                Start = "2022-06-17T02:00:00.000Z",
+                End = "2022-06-17T02:30:00.000Z"
+            };
+            var result = await _client.ExemplarQueryAsync(param);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Status, ResultStatuses.Success);
         }
     }
 }
