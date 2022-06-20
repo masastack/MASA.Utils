e0b746ad68289340e88d2d51a7b3b3919aa34367
MayueCif <6133287+MayueCif@users.noreply.github.com>
2022-06-20 15:07:22 +0800

    feat(authentication):add authentication oidc (#71)
    * feat:add masa oidc
    
    * feat:add authentication oidc
    
    * feat:update namespace
    
    * chore:merge error
    
    * chore:Misspelled word
    
    Co-authored-by: Mayue <mayue@lonsid.cn>

--- src/Security/Masa.Utils.Security.Authentication/Constants/MasaClaimTypes.cs
+++ src/Security/Masa.Utils.Security.Authentication/Constants/MasaClaimTypes.cs
@@ -1,15 +1,27 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
+// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 namespace Masa.Utils.Security.Authentication.Constants;
 
-public class MasaClaimTypes
+public struct MasaClaimTypes
 {
-    public const string MASA_PERMISSION = "https://masastack.com/security/authentication/MasaPermission";
+    public const string USER_NAME = ClaimTypes.Name;
 
-    public const string MASA_NICK_NAME = "https://masastack.com/security/authentication/MasaNickName";
+    public const string NAME = ClaimTypes.GivenName;
 
-    public const string MASA_DEPARTMENT_ID = "https://masastack.com/security/authentication/MasaDepartmentId";
+    public const string USER_ID = ClaimTypes.NameIdentifier;
 
-    public const string MASA_DEPARTMENT_NAME = "https://masastack.com/security/authentication/MasaDepartmentName";
+    public const string ROLE = ClaimTypes.Role;
+
+    public const string EMAIL = ClaimTypes.Email;
+
+    public const string PHONE_NUMBER = ClaimTypes.MobilePhone;
+
+    public const string EMAIL_VERIFIED = "https://masastack.com/security/authentication/email_verified";
+
+    public const string PHONE_NUMBER_VERIFIED = "https://masastack.com/security/authentication/phone_number_verified";
+
+    public const string ENVIRONMENT = "https://masastack.com/security/authentication/environment";
+
+    public const string TENANT = "https://masastack.com/security/authentication/tenant";
 }
