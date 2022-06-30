fe83cf269f4386dc8c15cbb3676d11e964478ee4
Qinyouzeng <102203523+Qinyouzeng@users.noreply.github.com>
2022-06-30 15:30:51 +0800

    Feat: Add Prometheus http api query client sdk (#78)
    * feat: add promethus http api query client
    
    * feat: add test project
    
    * feat: api query add
    
    * feat: update code and add tests
    
    * docs: add docs
    
    * feat: add convert json value to object
    
    * feat: fix enume and comments
    
    * feat: code styles and other specifications update
    
    * feat: update callerprovider inject
    
    * feat: update jsonelement convert to obj
    
    * feat: update extensions
    
    * feat: update internal to public
    
    * feat: update directory
    
    * feat: test refrence update
    
    * chore: comments and filename update
    
    * chore: remove not exists project id
    
    * chore: update docs and code style
    
    * chore: enums update
    
    * chore: doc code style update
    
    * chore: dosc update
    
    * chore: docs update
    
    * feat: add promethus http api query client
    
    * feat: add test project
    
    * feat: api query add
    
    * feat: update code and add tests
    
    * docs: add docs
    
    * feat: add convert json value to object
    
    * feat: fix enume and comments
    
    * feat: code styles and other specifications update
    
    * feat: update callerprovider inject
    
    * feat: update jsonelement convert to obj
    
    * feat: update extensions
    
    * feat: update internal to public
    
    * feat: update directory
    
    * feat: test refrence update
    
    * chore: comments and filename update
    
    * chore: remove not exists project id
    
    * chore: update docs and code style
    
    * chore: enums update
    
    * chore: doc code style update
    
    * chore: dosc update
    
    * chore: docs update
    
    * chore: name update and remove no used files
    
    * chore: folder name update
    
    * chore: file rename
    
    * chore: class split

---
+++ test/Masa.Utils.Data.Prometheus.Test/MasaPrometheusClientTests.cs
@@ -0,0 +1,116 @@
+// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Utils.Data.Prometheus.Test;
+
+[TestClass]
+public class MasaPrometheusClientTests
+{
+    private IMasaPrometheusClient _client;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        IServiceCollection service = new ServiceCollection();
+        service.AddPrometheusClient("http://localhost:9090");
+        _client = service.BuildServiceProvider().GetService<IMasaPrometheusClient>();
+    }
+
+    [TestMethod]
+    public async Task TestQueryAsync()
+    {
+        var result = await _client.QueryAsync(new QueryRequest
+        {
+            Query = "up"
+        });
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Status, ResultStatuses.Success);
+        Assert.IsNotNull(result.Data);
+    }
+
+    [TestMethod]
+    public async Task TestQueryVectorAsync()
+    {
+        var result = await _client.QueryAsync(new QueryRequest
+        {
+            Query = "up"
+        });
+
+        if (result != null && result.Data != null && result.Data.Result != null)
+        {
+            var data = result.Data.Result as QueryResultInstantVectorResponse[];
+
+            Assert.IsNotNull(data);
+            Assert.IsNotNull(data[0].Metric);
+            Assert.IsNotNull(data[0].Value);
+            Assert.IsNotNull(data[0].Metric.Keys);
+            Assert.AreEqual(2, data[0].Value.Length);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestQueryRangeAsync()
+    {
+        var result = await _client.QueryRangeAsync(new QueryRangeRequest
+        {
+            Query = "up",
+            Start = "2022-06-17T02:00:00.000Z",
+            End = "2022-06-17T02:30:00.000Z",
+            Step = "300s",
+        });
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Status, ResultStatuses.Success);
+        Assert.IsNotNull(result.Data);
+        if (result.Data.ResultType == ResultTypes.Matrix)
+        {
+            var data = result.Data.Result as QueryResultMatrixRangeResponse[];
+            Assert.IsNotNull(data);
+            Assert.IsNotNull(data[0].Metric);
+            Assert.IsNotNull(data[0].Values);
+        }
+    }
+
+    [TestMethod]
+    public async Task TestSeriesQueryAsync()
+    {
+        var result = await _client.SeriesQueryAsync(new MetaDataQueryRequest
+        {
+            Match = new string[] { "up" },
+            Start = "2022-06-17T02:00:00.000Z",
+            End = "2022-06-17T02:30:00.000Z"
+        });
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Status, ResultStatuses.Success);
+    }
+
+    [TestMethod]
+    public async Task TestLabelsQueryAsync()
+    {
+        var result = await _client.LabelsQueryAsync(default!);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Status, ResultStatuses.Success);
+    }
+
+    [TestMethod]
+    public async Task TestLabelValuesQueryAsync()
+    {
+        var result = await _client.LabelValuesQueryAsync(new LableValueQueryRequest());
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Status, ResultStatuses.Success);
+    }
+
+    [TestMethod]
+    [DataRow()]
+    public async Task TestExemplarQueryAsync()
+    {
+        var param = new QueryExemplarRequest
+        {
+            Query = "up",
+            Start = "2022-06-17T02:00:00.000Z",
+            End = "2022-06-17T02:30:00.000Z"
+        };
+        var result = await _client.ExemplarQueryAsync(param);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Status, ResultStatuses.Success);
+    }
+}
